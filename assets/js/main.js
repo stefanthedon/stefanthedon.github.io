d6c6b76ef80e815d91da5da1dd4310b8f2c3fe23
Donald Parminter Lokong <don.parminter@gmail.com>
2018-04-17 12:46:57 +0300

    #XD

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,19 @@
 function sendMail(str) {
     var link = "mailto:don.parminter@gmail.com" + "&subject=" + escape("New Contact Message") + "&body=" + escape(str);
+    fieldReset();
 }
 
 function submit_comment() {
-var Name = document.forms["contact_form"]["name"].value;
-var Email = document.forms["contact_form"]["email"].value;
-var Message = document.forms["contact_form"]["message"].value;
-if((Name !="") && (Email !="") && (Message !="")) {
-   sendMail("HELLO");
+    var Name = document.querySelector('#name').value;
+    var Email = document.querySelector('#email').value;
+    var Message = document.querySelector('#message').value;
+    if((Name !="") && (Email !="") && (Message !="")) {
+        sendMail("HELLO");
+    }
 }
+
+function fieldReset() {
+    Name.value = '';
+    Email.value = '';
+    Message.value = '';
 }
